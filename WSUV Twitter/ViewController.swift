fdb4004f05b33c644180e9d9f58b967070e1ccbb
ssmiler <spencer.kitchen@wsu.edu>
2017-04-05 21:51:34 -0700

    AlamoFire working, working on ui

--- WSUV Twitter/ViewController.swift
+++ WSUV Twitter/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        Alamofire.request("https://httpbin.org/get").responseJSON { response in
+            print(response.request!)  // original URL request
+            print(response.response!) // HTTP URL response
+            print(response.data!)     // server data
+            print(response.result)   // result of response serialization
+            
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+        }
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
